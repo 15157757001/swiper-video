1d39a0dce54c541cfa0a2ece948971934ae826cb
mxphpxm <a949600520@qq.com>
2019-08-19 10:08:53 +0800

    u:'新增进度条'

--- components/chunlei-video/chunlei-video.vue
+++ components/chunlei-video/chunlei-video.vue
@@ -1,11 +1,15 @@
 <template>
-	<video :src="src" :controls="false" :show-play-btn="false" 
-		:enable-progress-gesture="false" @click="clickVideo"
-		id="video" ref="video" class="video"></video>
+	<view>
+		<video :src="src" :controls="false" :show-play-btn="false" 
+			:style="{ height: height,width: width }" :loop="true"
+			:enable-progress-gesture="false" @click="clickVideo"
+			:id="`video_${src}`" ref="`video_${src}`" class="video" @timeupdate="timeupdate"></video>
+		<cover-view class="progressBar" :style="{ width: barWidth }"></cover-view>
+	</view>
 </template>
 
 <script>
-	export default{
+	export default{ 
 		props:{
 			src:{
 				type:String,
@@ -14,30 +18,54 @@
 			play:{
 				type:Boolean,
 				default:false
+			},
+			height:{
+				type:String,
+				default:''
+			},
+			width:{
+				type:String,
+				default:''
+			},
+			duration:{
+				type:Number,
+				default:0
 			}
 		},
 		data(){
 			return{
-				
+				time:0
 			}
 		},
 		methods:{
+			timeupdate(event){
+				if(this.time>=this.duration) this.time=0
+				this.time = this.time + 0.25 
+				
+			},
 			clickVideo(){
 				this.$emit('click')
 			},
 			videoPlay(){
 				if(this.play){
-					this.videoCtx = uni.createVideoContext('video',this);
+					this.videoCtx = uni.createVideoContext(`video_${this.src}`,this);
 					this.videoCtx.play();
 				}else{
-					this.videoCtx = uni.createVideoContext('video',this);
+					this.videoCtx = uni.createVideoContext(`video_${this.src}`,this);
 					this.videoCtx.pause();
 				}
+				
 			}
 		},
 		watch:{
 			play(newVal,oldVal){
 				this.videoPlay()
+			}
+		},
+		computed:{
+			barWidth(){
+				let width = this.time/this.duration*parseInt(this.width)
+				return `${width}px`
 			}
 		}
 	}
@@ -48,4 +76,28 @@
 		width: 100%;
 		height: 100%;
 	}
+	.progressBar{
+		border-radius: 2upx;
+		height: 4upx;
+		background-color: #FFFFFF;
+		z-index: 999999;
+		position: absolute;
+		bottom: 40upx;
+		//#ifndef APP-PLUS-NVUE
+		animation: flicker 4s linear infinite;
+		animation-direction:alternate;
+		//#endif
+	}
+	//#ifndef APP-PLUS-NVUE
+	@keyframes flicker {
+		0% { box-shadow:0 0 0 #FFFFFF; }
+	     /** 暂停效果 */
+		10% { box-shadow:0 0 2upx #FFFFFF; }
+	    50% { box-shadow:0 0 10upx #FFFFFF; }
+	    60% { box-shadow:0 0 12upx #FFFFFF; }
+	    90% { box-shadow:0 0 18upx #FFFFFF; }
+	    100% { box-shadow:0 0 20upx #FFFFFF; }
+	
+	}
+	//#endif
 </style>
